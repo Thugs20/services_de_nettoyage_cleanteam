0f8897d8ab03cbbecc60ae1b9c8f00900e743609
HOUETO Fabrice <fbrchoueto@gmail.com~>
2026-03-06 13:08:39 +0100

    Ajout de la section About

--- script.js
+++ script.js
@@ -161,3 +161,27 @@ languageSwitcher.addEventListener("change", function () {
 /* 9️⃣ Initialisation */
 updateTexts();
 showHeroSlide(currentSlide);
+
+/* ======================================
+   SCROLL FLUIDE ENTRE LES SECTIONS
+====================================== */
+
+const navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach(link => {
+
+link.addEventListener("click", function(e){
+
+e.preventDefault();
+
+const target = document.querySelector(this.getAttribute("href"));
+
+target.scrollIntoView({
+behavior:"smooth"
+});
+
+navMenu.classList.remove("active");
+
+});
+
+});
